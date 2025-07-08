c394e88cc7636c7845e4907d61966abc91b54b2a
p1geon <maxym.shmurygin@fyst.com>
2025-07-08 15:38:13 -0400

    modified

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -681,7 +681,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
@@ -963,7 +963,18 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
+  {
+    'sindrets/diffview.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
+    keys = {
+      { '<leader>gd', '<cmd>DiffviewOpen<CR>', desc = 'Git Diff (Diffview)' },
+      { '<leader>gh', '<cmd>DiffviewFileHistory<CR>', desc = 'Git File History' },
+    },
+    config = function()
+      require('diffview').setup()
+    end,
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
@@ -977,7 +988,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,11 +15,35 @@ return {
   },
   opts = {
     filesystem = {
+      follow_current_file = {
+        enabled = true, -- focus follows the file you're editing
+      },
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          -- ['\\'] = 'close_window',
+          -- ['P'] = 'toggle_preview', -- preview file on the side
+          -- ['l'] = 'open', -- open file or expand folder
+          -- ['h'] = 'close_node', -- collapse folder
+          -- ['<space>'] = 'toggle_node', -- fold/unfold node
         },
       },
+      use_libuv_file_watcher = true, -- detects file changes without manual refresh
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true, -- shows  and  for collapsed/expanded folders
+        expander_collapsed = '',
+        expander_expanded = '',
+      },
+      icon = {
+        folder_closed = '',
+        folder_open = '',
+        default = '',
+      },
+      -- modified = {
+      --   symbol = '[+]',
+      --   highlight = 'NeoTreeModified',
+      -- },
     },
   },
 }
